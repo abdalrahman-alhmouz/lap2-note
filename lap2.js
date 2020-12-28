48a641d5a15079f722d716f0a93165f6d7317088
abdalrahman-alhmouz <abood.alhmouz121@gmail.com>
2020-12-28 16:03:41 +0200

    init comit

---
+++ lap2.js
@@ -0,0 +1,34 @@
+prompt("what is your name ?");
+var name = prompt("what is your name ?");
+alert(name);
+alert("hello"+name);
+
+var userInput = prompt("what is my name (abdalrahman or ahmad)?").toLowerCase(userInput);
+switch(userInput){
+case 'abdalrahman' :
+    alert("that true");
+break;
+case 'ahmad':
+alert('that false');
+break ;
+
+}
+var userInput2 = prompt("what is my hoppy (sport or drawing)?").toLowerCase(userInput2);
+switch(userInput2){
+case 'sport' :
+    alert("that true");
+break;
+case 'drawing':
+alert('that false');
+break ;
+
+}  
+var myTeam = prompt("Is my hair black?(yes or no )").toLowerCase(myteam);
+if (myTeam === "yes") {
+    alert('true');}
+    // console.log("true");
+else {
+    alert('false');
+    //console.log("false");
+}
+
